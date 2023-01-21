8d9b018e0bcd307108c77586eb5b4fa9f8a3e8d6
Nabeel <16357000+Nabizzle@users.noreply.github.com>
2023-01-21 12:44:05 -0500

    docs: Adds GoPro disclaimer
    Adds the disclaimer that Open GoPro asks you to add to the code.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='GoPro App',
     version='1.0.0',
     description='App to control the video from a GoPro',
-    long_description='''Application to set video parameters and to start and stop video wirelessly. This is meant to preserve the battery life of the GoPro for as long as possible without having to directly interact with the GoPro.''',
+    long_description='''Application to set video parameters and to start and stop video wirelessly. This is meant to preserve the battery life of the GoPro for as long as possible without having to directly interact with the GoPro. This product and/or service is not affiliated with, endorsed by or in any way associated with GoPro Inc. or its products and services. GoPro, HERO, and their respective logos are trademarks or registered trademarks of GoPro, Inc.''',
     author='Nabeel Chowdhury',
     author_email='nabeel.chowdhury@case.edu',
     license = "MIT",
