069bed090d28adce8e914d192f2a6a224ca50702
Nabeel <16357000+Nabizzle@users.noreply.github.com>
2023-01-25 13:06:19 -0500

    updates setup.py to most current release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="GoPro App",
-    version="1.2.0",
+    version="1.2.1",
     author="Nabeel Chowdhury",
     author_email="nabeel.chowdhury@case.edu",
     py_modules=["GoPro App"],
